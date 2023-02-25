1c32f72f3bcbaac75e623b62669d02fd52c1704f
alexalehno <alekswakeup@gmail.com>
2023-02-25 18:30:52 +0300

    refactor: change code_wars.js

--- js/code_wars.js
+++ js/code_wars.js
@@ -2,16 +2,17 @@
 
 
 function startCodeWars() {
-  const url = `https://www.codewars.com/api/v1/users/alexalehno/`;
-  const url1 = `https://www.codewars.com/api/v1/users/alexalehno/code-challenges/completed`;
-  const url2 = `https://www.codewars.com/api/v1/code-challenges/`;
+  const urlUser = `https://www.codewars.com/api/v1/users/alexalehno/`;
+  const urlCompleted = `https://www.codewars.com/api/v1/users/alexalehno/code-challenges/completed`;
+  const urlDescription = `https://www.codewars.com/api/v1/code-challenges/`;
   const overlay = document.querySelector('.overlay-preloader');
   let elemUl = null;
 
 
-  getInfo(url).then(data => {
+  getInfo(urlUser).then(data => {
     const info = [data.username, data.honor, data.ranks.overall.name, data.codeChallenges.totalCompleted];
     const items = document.querySelectorAll('.info-item');
+
     items.forEach((el, i) => el.lastChild.textContent = info[i]);
     overlay.classList.add('hidden');
   })
@@ -19,7 +20,7 @@ function startCodeWars() {
   document.querySelector('#completed-btn').addEventListener('click', () => {
     overlay.classList.remove('hidden');
 
-    getInfo(url1).then(data => {
+    getInfo(urlCompleted).then(data => {
       const dataList = data.data;
       const ul = createEl('ul', 'kata-list');
       let str = '';
@@ -28,13 +29,26 @@ function startCodeWars() {
       elemUl = ul;
 
       for (let i = 0; i < dataList.length; i++) {
-        let dateStr = new Date(dataList[i].completedAt).toLocaleDateString('en', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' });
+        let dateStr = new Date(dataList[i].completedAt).toLocaleDateString('en', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' });
 
-        str += `<li data-slug="${dataList[i].slug}">
-        <div class="info-wrap"><span>&#8470;</span><span>${i + 1}</span></div>
-        <div class="info-wrap"><span>Name</span><span>${dataList[i].name}</span></div>
-        <div class="info-wrap"><span>Language</span><span>${dataList[i].completedLanguages}</span></div>
-        <div class="info-wrap"><span>Completed</span><span>${dateStr}</span></div>
+        str += `
+        <li data-slug="${dataList[i].slug}">
+          <div class="info-wrap">
+            <span>&#8470;</span>
+            <span>${i + 1}</span>
+          </div>
+          <div class="info-wrap">
+            <span>Name</span>
+            <span>${dataList[i].name}</span>
+          </div>
+          <div class="info-wrap">
+            <span>Language</span>
+            <span>${dataList[i].completedLanguages}</span>
+          </div>
+          <div class="info-wrap">
+            <span>Completed</span>
+            <span>${dateStr}</span>
+          </div>
         </li>`;
       }
 
@@ -44,25 +58,26 @@ function startCodeWars() {
 
       return elemUl;
 
-    }).then(ul => {
+    }).then(elemUl => {
 
       let elemDiv = null;
 
-      ul.addEventListener('click', (e) => {
+      elemUl.addEventListener('click', (e) => {
         overlay.classList.remove('hidden');
 
         let li = e.target.closest('li');
 
-        getInfo(url2 + li.dataset.slug).then(data => {
+        getInfo(urlDescription + li.dataset.slug).then(data => {
           const div = createEl('div', 'overlay overlay-kata');
 
           if (elemDiv) elemDiv.remove();
           elemDiv = div;
 
-          elemDiv.innerHTML = `<div class="kata-content">
-          <h3>${data.name}</h3>
-          <p><span><b>Rank:</b></span> ${data.rank.name}</p>
-          <p>${data.description}</p>
+          elemDiv.innerHTML = `
+          <div class="kata-content">
+            <h3>${data.name}</h3>
+            <p><span><b>Rank:</b></span> ${data.rank.name}</p>
+            <p>${data.description}</p>
           </div>`;
 
           document.querySelector('.task-wrap').append(div);
